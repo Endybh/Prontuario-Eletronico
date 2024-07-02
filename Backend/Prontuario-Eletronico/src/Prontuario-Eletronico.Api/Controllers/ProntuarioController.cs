932c2b7833f672a107cdfa9187553d33d191bf16
Endybh <enderson.demaria@gmail.com>
2024-07-02 11:35:00 -0300

    Primeiro commit do projeto

---
+++ Backend/Prontuario-Eletronico/src/Prontuario-Eletronico.Api/Controllers/ProntuarioController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Prontuario_Eletronico.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProntuarioController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Abrir()
+        {
+            return Ok();
+        }
+    }
+}
