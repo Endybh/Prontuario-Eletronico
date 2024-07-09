44270451e5b1a0b0c7578990a8e2453fedf94d83
Endybh <enderson.demaria@gmail.com>
2024-07-09 15:06:34 -0300

    Tradução do projeto para inglês

---
+++ Backend/Prontuario-Eletronico/src/Prontuario-Eletronico.Core/Aplicacao/Commands/AdicionarProfissionalCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Prontuario_Eletronico.Core.Entities;
+
+namespace Prontuario_Eletronico.Core.Application
+{
+    public class AddProfessionalCommand : IRequest<Professional>
+    {
+        public string Name { get; set; }
+        public string FuncionalNumber{ get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Backend/Prontuario-Eletronico/src/Prontuario-Eletronico.Core/Responses/PostResponseDefault.cs
@@ -0,0 +1,7 @@
+﻿namespace Prontuario_Eletronico.Core;
+
+public class PostResponseDefault
+{
+    public Guid EntityId { get; set; }
+    public string Message { get; set; }
+}
